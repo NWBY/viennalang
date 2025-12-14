dad84f1ef47b33ce762fc8067ba841458bcac0ce
Sam Newby <sam.newby19@gmail.com>
2025-12-14 00:45:03 +0000

    chore: add the rest of token types

--- src/lexer/token.zig
+++ src/lexer/token.zig
@@ -1,1 +1,38 @@
-pub const TokenType = enum { INT, STRING, TRUE, FALSE };
+pub const TokenType = enum {
+    INT,
+    STRING,
+    TRUE,
+    FALSE,
+
+    // Keywords
+    CONST,
+    VAR,
+    FUNC,
+    RETURN,
+    IF,
+    ELSE,
+
+    // Operators
+    PLUS,
+    MINUS,
+    STAR,
+    SLASH,
+    ASSIGN,
+    SEMICOLON,
+
+    // Delimiters
+    LPAREN,
+    RPAREN,
+    LBRACE,
+    RBRACE,
+
+    // Other
+    IDENT,
+    EOF,
+};
+
+pub const Token = struct {
+    type: TokenType,
+    lexeme: []const u8,
+    line: usize,
+};
