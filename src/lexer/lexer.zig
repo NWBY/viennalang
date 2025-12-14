690dba336dcaa6ce9e8a7a080de13bdf0cf85300
Sam Newby <sam.newby19@gmail.com>
2025-12-14 00:56:22 +0000

    chore: adding some sustinance to lexer

--- src/lexer/lexer.zig
+++ src/lexer/lexer.zig
@@ -14,4 +14,35 @@ pub const Lexer = struct {
             .line = 1,
         };
     }
+
+    fn isAtEnd(self: *Lexer) bool {
+        return self.current >= self.source.len;
+    }
+
+    fn advance(self: *Lexer) u8 {
+        const c = self.source[self.current];
+        self.current += 1;
+        return c;
+    }
+
+    fn peek(self: *Lexer) u8 {
+        if (self.isAtEnd()) return 0;
+        return self.source[self.current];
+    }
+
+    fn makeToken(self: *Lexer, tokenType: TokenType) Token {
+        return Token{
+            .type = tokenType,
+            .lexeme = self.source[self.start - 1 .. self.current],
+            .line = self.line,
+        };
+    }
+
+    fn errorToken(self: *Lexer, message: []const u8) Token {
+        return Token{
+            .type = TokenType.EOF, // TODO: make a proper error token type
+            .lexeme = message,
+            .line = self.line,
+        };
+    }
 };
